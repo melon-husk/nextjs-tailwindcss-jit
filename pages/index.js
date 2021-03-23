7c65c2d066eb78c1cc81b4483330675dd92b98bf
melon-husk <ujwal5555@gmail.com>
2021-03-24 04:41:36 +0530

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <h1 className="font-bold text-[3rem] ">Hello World !</h1>
+    </div>
+  );
+}
